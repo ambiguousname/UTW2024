a068104356010375ceb4f36b762f710d8752e59a
Tyler K <ambiguousemails@proton.me>
2024-10-12 20:33:26 -0700

    Getting rid of comments

--- lib.h
+++ lib.h
@@ -1,16 +1,9 @@
 #ifndef __LIB_H__
 #define __LIB_H__
 #include <stdint.h>
-// Conversion. Must always write additional definitions.
 
-// Representation. 
-// In Rust, this is the difference between c_char and char.
-// In JS/WASM, strings must be converted to and from binary form.
 char get_second_character(const char* str);
 
-// Typing differences.
-// In C, this can be something small like i32 vs. int32_t
-// In JS, this is something like the fact that enums are not natively supported.
 int8_t addition(int8_t a, int8_t b);
 
 struct PaddingStructExample {
